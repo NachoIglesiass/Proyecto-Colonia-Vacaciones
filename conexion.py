9e0dce361150ffb7d4cc3180016ce09548dc58c4
Nachoiglesiass <nachoiglesias36@gmail.com>
2024-12-16 23:32:27 -0300

    Actualiza configuración de conexión a la base de datos

--- conexion.py
+++ conexion.py
@@ -1,13 +1,14 @@
 import mysql.connector
+import os
 
 def conectar_bdd():
-    # Conexiòn
+    # Conexión
     database = mysql.connector.connect(
-    host = "localhost",
-    user = "nacho",
-    password = "43639241ni",
-    database = "colonia_vacaciones",
-    port = 3306
+        host=os.environ.get('DB_HOST'),  # Cambia esto por el host de tu base de datos en la nube
+        user=os.environ.get('DB_USER'),  # Cambia esto por tu usuario
+        password=os.environ.get('DB_PASSWORD'),  # Cambia esto por tu contraseña
+        database=os.environ.get('DB_NAME'),  # Cambia esto por el nombre de tu base de datos
+        port=3306  # Asegúrate de que el puerto sea correcto
     )
     # Cursor
     cursor = database.cursor(buffered=True)
